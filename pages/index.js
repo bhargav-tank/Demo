9cb9053e92266cc53a2e9cb8a61d2db9f44330b3
bhargav-tank <bhargavtank0805@gmail.com>
2024-02-07 10:24:31 +0530

    stages

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../componets/Header";
+import AccessibilityNewIcon from "@material-ui/icons/AccessibilityNew";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <AccessibilityNewIcon />
+    </>
+  );
+};
+
+export default Home;
